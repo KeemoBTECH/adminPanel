bb5b5fca9bb759bdad975fdaadfa43a6a164b46b
KeemoBTECH <xemail944@gmail.com>
2026-08-29 23:45:09 +0300

    Fix API URL environment variable name

--- admin/lib/api.ts
+++ admin/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'kinangotti-api-production.up.railway.app/api';
+const API_URL = process.env.NEXT_PUB_API_URL || 'https://kinangotti-api-production.up.railway.app/api';
 
 const api = axios.create({
     baseURL: API_URL,
